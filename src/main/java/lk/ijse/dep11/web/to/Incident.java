0c32c15fb3a1ace1577bbbf39b09b4acba52c0af
Isuru Ashinsana <isuruashinsana@gmail.com>
2023-11-11 12:33:39 +0530

    create serialized incident class

---
+++ src/main/java/lk/ijse/dep11/web/to/Incident.java
@@ -0,0 +1,20 @@
+package lk.ijse.dep11.web.to;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Incident implements Serializable {
+    private String id;
+    private Date date;
+    private String incident;
+    private String description;
+    private String location;
+    private String reporting_person;
+    private String pictureUrl;
+}

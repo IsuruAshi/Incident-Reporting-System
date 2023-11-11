d2e03c98abbad2376456152d0507f18da04c50c8
Isuru Ashinsana <isuruashinsana@gmail.com>
2023-11-11 12:08:34 +0530

    create tables incident &  picture

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE incident(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    date DATE NOT NULL ,
+    incident_name VARCHAR(100) NOT NULL ,
+    description VARCHAR(500) NOT NULL ,
+    location VARCHAR(100) NOT NULL ,
+    reporting_person VARCHAR(100) NOT NULL
+);
+CREATE TABLE picture(
+    incident_id INT PRIMARY KEY ,
+    path VARCHAR(1000) NOT NULL ,
+    CONSTRAINT fk_picture FOREIGN KEY (incident_id) REFERENCES incident(id)
+                    ON UPDATE CASCADE
+);
